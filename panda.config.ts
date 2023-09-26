ea7913d8253d58fbb5c819a1ad36777dc6506256
anubra266 <anubra266@gmail.com>
2023-09-26 23:23:13 +0100

    chore: update studio config

--- panda.config.ts
+++ panda.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
+  studio: {
+    inject: {
+      head: `<title>EcoPanda Studio</title> <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐼</text></svg>">`,
+    },
+  },
   preflight: true,
 
   hash: true,
